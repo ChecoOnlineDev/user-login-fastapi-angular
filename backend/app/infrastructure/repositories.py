d5fea968a30f5066ba86f61ca10da20a319d825f
Sergio Alejandro De la Cruz Garcia <checodlcg@gmail.com>
2025-11-27 17:03:52 -0600

    minor fixes

--- backend/app/infrastructure/repositories.py
+++ backend/app/infrastructure/repositories.py
@@ -8,22 +8,22 @@ class SqlAlchemyUserRepository(UserRespository):
         self.db_session = db_session
         
         #busca al usuario por email, si existe lo retorna como UserResponse, si no retorna None
-        def get_user_by_email(self, email: str) -> UserResponse | None:
-            user_exists = self.db_session.query(UserModel).filter(UserModel.email == email).first() #busca el usuario por email
-            if user_exists:
-                return UserResponse.model_validate(user_exists)
-            return None
+    def get_user_by_email(self, email: str) -> UserResponse | None:
+        user_exists = self.db_session.query(UserModel).filter(UserModel.email == email).first() #busca el usuario por email
+        if user_exists:
+            return UserResponse.model_validate(user_exists)
+        return None
         
         #crea un nuevo usuario en la base de datos y retorna al usuario creado como UserResponse
-        def create_user(self, user_create: UserCreate) -> UserResponse:
-            new_user = UserModel(
-                email=user_create.email,
-                hashed_password=user_create.password,
-                full_name = user_create.full_name,
-                is_active = user_create.is_active
-            )
-            self.db_session.add(new_user)
-            self.db_session.commit()
-            self.db_session.refresh(new_user)
+    def create_user(self, user_create: UserCreate) -> UserResponse:
+        new_user = UserModel(
+            email=user_create.email,
+            hashed_password=user_create.password,
+            full_name = user_create.full_name,
+            is_active = user_create.is_active
+        )
+        self.db_session.add(new_user)
+        self.db_session.commit()
+        self.db_session.refresh(new_user)
             
-            return UserResponse.model_validate(new_user)
+        return UserResponse.model_validate(new_user)
